bb04c183bcfdf9be207cbdfe6aa91fbe116a56c0
fajarfaz <fajarfaz@gmail.com>
2022-11-08 00:39:23 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'], // aktifkan cors
 
     'allowed_methods' => ['*'],
 
